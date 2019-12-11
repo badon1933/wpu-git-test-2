0db7d4a690c8b5e4a5a1f20bcf751b5d2ffa92a2
firdaus <badon1933@gmail.com>
2019-12-11 09:36:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto vel accusantium, repellendus suscipit error aspernatur sit obcaecati quod, porro qui animi, id eum. Sed, eius. Autem voluptas harum aliquid accusantium.</p>
+  <ul>
+    <li>Apa saja</li>
+    <li>Apa saja 2</li>
+  </ul>
 </body>
 
 </html>
